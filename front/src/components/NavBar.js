a1165cec17b36d4c7152754ef9c155eaecb92d5a
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-03-10 16:24:37 +0100

    navbar basique presque ok

--- front/src/components/NavBar.js
+++ front/src/components/NavBar.js
@@ -4,9 +4,15 @@ import { Resume } from "./Resume"
 export const NavBar = () => {
   return (
     <div className="navbar">
-      <div className="navbar-button">01</div>
-      <div className="navbar-button">02</div>
-      <div className="navbar-button">03</div>
+      <div className="navbar-button">
+        <span>01. <a>About</a></span>
+      </div>
+      <div className="navbar-button">
+        <span>02. <a>Projects</a></span>
+      </div>
+      <div className="navbar-button">
+        <span>03. <a>Contact</a></span>
+      </div>
       <Resume />
     </div>
   )
