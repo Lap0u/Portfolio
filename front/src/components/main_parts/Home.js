16d3fb69be358a7ea87cccb10d85847065821572
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-03-14 20:30:06 +0100

    home

--- front/src/components/main_parts/Home.js
+++ front/src/components/main_parts/Home.js
@@ -1,7 +1,10 @@
+import './Home.scss'
+
 export const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <p className="smaller-intro">Hello I am</p>
+      <h1>Clément Beaurain.</h1>
       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt accusamus, iusto tempora aspernatur blanditiis, totam sequi omnis eos aliquid debitis dignissimos consectetur dolores quam, quas ratione dicta aliquam in nam!</div>
     </div>
   )
