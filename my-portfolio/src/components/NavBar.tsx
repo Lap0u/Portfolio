636d6571928cf2a1d9bcda76169875ff5aea49cc
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-07-16 14:31:41 +0200

    Navbar mobile done

--- my-portfolio/src/components/NavBar.tsx
+++ my-portfolio/src/components/NavBar.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useBreakpoints } from '../hooks/useBreakpoints';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 const NavBar = () => {
   const { isMobile } = useBreakpoints();
+  const [isNavVisible, setNavVisible] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
+  };
+
+  const toggleNavBar = () => {
+    setNavVisible(!isNavVisible);
   };
 
   const handleMouseLeave = () => {
@@ -19,7 +25,45 @@ const NavBar = () => {
   return (
     <>
       {isMobile ? (
-        <div>mobile nav</div>
+        <div className="w-screen flex justify-between p-4 radial1 fixed items-center">
+          <div className="hover:opacity-80 font-roboto text-2xl text-pastelTeal w-[10vw] font-bold self-start">
+            Clément<span className="text-pastelBlue">__B.</span>
+          </div>
+          <div className="flex flex-col relative justify-right">
+            <Icon
+              onClick={toggleNavBar}
+              icon="bxs:left-arrow"
+              className={
+                'text-white self-end' +
+                (isNavVisible ? ' opacity-50 -rotate-90' : '')
+              }
+            />
+            {isNavVisible && (
+              <div className="flex flex-col gap-y-4">
+                <Link
+                  to="home"
+                  className="font-roboto text-md text-white font-semibold text-right">
+                  home
+                </Link>
+                <Link
+                  to="competences"
+                  className="font-roboto text-md text-white font-semibold text-right">
+                  compétences
+                </Link>
+                <Link
+                  to="projets"
+                  className="font-roboto text-md text-white font-semibold text-right">
+                  projets
+                </Link>
+                <Link
+                  to="contact"
+                  className="font-roboto text-md text-white font-semibold text-right">
+                  contact
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
       ) : (
         <div className="w-screen flex justify-center items-center z-10 top-0 radial1 pb-6 fixed">
           <div className="hover:opacity-80 font-roboto text-2xl text-pastelTeal w-[10vw] font-bold absolute top-10 left-10">
