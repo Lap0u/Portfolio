e232cb8d84a5e19678bd5a72ab380053f8952671
Clement Beaurain <clementbeaurainpro@gmail.com>
2023-07-16 13:43:38 +0200

    init ismobile ok

--- my-portfolio/src/components/NavBar.tsx
+++ my-portfolio/src/components/NavBar.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useBreakpoints } from '../hooks/useBreakpoints';
 
 const NavBar = () => {
+  const { isMobile } = useBreakpoints();
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
@@ -15,41 +17,47 @@ const NavBar = () => {
     isHovered ? 'opacity-40' : 'opacity-100'
   }`;
   return (
-    <div className="w-screen flex justify-center items-center z-10 top-0 radial1 pb-6 fixed">
-      <div className="hover:opacity-80 font-roboto text-2xl text-pastelTeal w-[10vw] font-bold absolute top-10 left-10">
-        Clément<span className="text-pastelBlue">__B.</span>
-      </div>
-      <div className="flex justify-center items-center gap-x-10 mt-10">
-        <Link
-          to="home"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className={elemClasses}>
-          // home
-        </Link>
-        <Link
-          to="competences"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className={elemClasses}>
-          // compétences
-        </Link>
-        <Link
-          to="projets"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className={elemClasses}>
-          // projets
-        </Link>
-        <Link
-          to="contact"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className={elemClasses}>
-          // contact
-        </Link>
-      </div>
-    </div>
+    <>
+      {isMobile ? (
+        <div>mobile nav</div>
+      ) : (
+        <div className="w-screen flex justify-center items-center z-10 top-0 radial1 pb-6 fixed">
+          <div className="hover:opacity-80 font-roboto text-2xl text-pastelTeal w-[10vw] font-bold absolute top-10 left-10">
+            Clément<span className="text-pastelBlue">__B.</span>
+          </div>
+          <div className="flex justify-center items-center gap-x-10 mt-10">
+            <Link
+              to="home"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className={elemClasses}>
+              // home
+            </Link>
+            <Link
+              to="competences"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className={elemClasses}>
+              // compétences
+            </Link>
+            <Link
+              to="projets"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className={elemClasses}>
+              // projets
+            </Link>
+            <Link
+              to="contact"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className={elemClasses}>
+              // contact
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
